e78cbca3201fad8163216f45c3e4b9284fbb3fe9
Adrian Haarbach <mail@adrian-haarbach.de>
2020-06-24 21:00:22 +0200

    rendering voxels using matplotlib

--- render.py
+++ render.py
@@ -7,3 +7,10 @@ expressions = ["cy(48,48,32,8,12)cu(24,24,40,28)+"]#, "sp(48,48,32,8,12)cu(24,24
 
 voxels = voxels_from_expressions(expressions, primitives, max_len=7)
 print(voxels.shape)
+
+import matplotlib.pyplot as plt
+import numpy as np
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.voxels(np.squeeze(voxels), edgecolor='k')
+plt.show()
